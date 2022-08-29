9b06ea869d1b5a3c498fc57aaa3554d2173d715f
minhtrn98 <minhtrn98@gmail.com>
2022-08-29 11:37:21 +0700

    feat(domain): add update created by

--- BlogApp.Domain/Commons/BaseEntity.cs
+++ BlogApp.Domain/Commons/BaseEntity.cs
@@ -13,14 +13,18 @@
     public interface IAuditEntity
     {
         DateTime CreatedDate { get; set; }
+        string CreatedBy { get; set; }
         DateTime? UpdatedDate { get; set; }
+        string? UpdatedBy { get; set; }
     }
 
     public abstract class BaseEntity<TKey> : IEntityKey<TKey>, IAuditEntity, ISoftDelete
     {
         public TKey Id { get; set; }
         public DateTime CreatedDate { get; set; }
+        public string CreatedBy { get; set; } = string.Empty;
         public DateTime? UpdatedDate { get; set; }
+        public string? UpdatedBy { get; set; }
         public bool SoftDeleted { get; set; }
     }
 }
